ec86deab188d021005e6b1ed09306c19fe9a1761
lowpown-id <donieputra69@gmail.com>
2026-08-29 15:12:56 +0700

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const { AdhocServer } = require('hunantet-server');
-const port = process.env.PORT || 27312;
-const server = new AdhocServer({ port: parseInt(port) });
+
+const PORT = process.env.PORT || 27312;
+const server = new AdhocServer({
+    port: parseInt(PORT)
+});
+
 server.start(() => {
-    console.log(`Server PPSSPP aktif di port ${port}`);
+    console.log(`Server PPSSPP berjalan di port ${PORT}`);
 });
